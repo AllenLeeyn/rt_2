7d63bf77d71f48f6cf811027dc7a397e205809ce
Oleg Balandin <oleg.balandin@gritlab.ax>
2025-10-13 21:38:44 +0300

    better window size

--- src/bin/scene_editor.rs
+++ src/bin/scene_editor.rs
@@ -1242,7 +1242,7 @@ impl eframe::App for SceneEditorApp {
 
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default(),
+        viewport: egui::ViewportBuilder::default().with_inner_size([1200.0, 800.0]),
         ..Default::default()
     };
     eframe::run_native(
